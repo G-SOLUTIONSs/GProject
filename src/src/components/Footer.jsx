0ecad2684c0258d0c9e3f7390695195cd169e48d
Ruben Dominguez Garcia <58381215+RubenDomiguezG@users.noreply.github.com>
2025-07-20 22:06:02 -0600

    Delete src directory

--- src/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-  <footer className="bg-rojo_c text-white text-center p-4 mt-10">
-    <p>&copy; 2025 G Solutions. Todos los derechos reservados.</p>
-  </footer>
-)
-
-export default Footer
